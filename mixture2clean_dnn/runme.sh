c4c45db8c477ffd0db9581d47aee98f2d03dc30d
xingda <dxingda@gmail.com>
2018-05-10 17:34:11 -0700

    runme

--- mixture2clean_dnn/runme.sh
+++ mixture2clean_dnn/runme.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -e
 
 MINIDATA=1
 if [ $MINIDATA -eq 1 ]; then
   WORKSPACE="workspace"
-  mkdir $WORKSPACE
+  mkdir -p $WORKSPACE
   TR_SPEECH_DIR="mini_data/train_speech"
   TR_NOISE_DIR="mini_data/train_noise"
   TE_SPEECH_DIR="mini_data/test_speech"
@@ -29,24 +30,25 @@ python prepare_data.py calculate_mixture_features --workspace=$WORKSPACE --speec
 python prepare_data.py calculate_mixture_features --workspace=$WORKSPACE --speech_dir=$TE_SPEECH_DIR --noise_dir=$TE_NOISE_DIR --data_type=test --snr=$TE_SNR
 
 # Pack features. 
-N_CONCAT=7
-N_HOP=3
-python prepare_data.py pack_features --workspace=$WORKSPACE --data_type=train --snr=$TR_SNR --n_concat=$N_CONCAT --n_hop=$N_HOP
-python prepare_data.py pack_features --workspace=$WORKSPACE --data_type=test --snr=$TE_SNR --n_concat=$N_CONCAT --n_hop=$N_HOP
+N_CONCAT=1
+N_HOP=1
+N_NOISE_FRAME=6
+python prepare_data.py pack_features --workspace=$WORKSPACE --data_type=train --snr=$TR_SNR --n_concat=$N_CONCAT --n_hop=$N_HOP --noise_frame=$N_NOISE_FRAME
+python prepare_data.py pack_features --workspace=$WORKSPACE --data_type=test --snr=$TE_SNR --n_concat=$N_CONCAT --n_hop=$N_HOP --noise_frame=$N_NOISE_FRAME
 
 # Compute scaler. 
-python prepare_data.py compute_scaler --workspace=$WORKSPACE --data_type=train --snr=$TR_SNR
+# python prepare_data.py compute_scaler --workspace=$WORKSPACE --data_type=train --snr=$TR_SNR
 
 # Train. 
 LEARNING_RATE=1e-4
-CUDA_VISIBLE_DEVICES=3 python main_dnn.py train --workspace=$WORKSPACE --tr_snr=$TR_SNR --te_snr=$TE_SNR --lr=$LEARNING_RATE
+ITERATION=1000
+CUDA_VISIBLE_DEVICES=3 python main_dnn.py train --workspace=$WORKSPACE --tr_snr=$TR_SNR --te_snr=$TE_SNR --lr=$LEARNING_RATE --iter=$ITERATION
 
 # Plot training stat. 
-python evaluate.py plot_training_stat --workspace=$WORKSPACE --tr_snr=$TR_SNR --bgn_iter=0 --fin_iter=10001 --interval_iter=1000
+python evaluate.py plot_training_stat --workspace=$WORKSPACE --tr_snr=$TR_SNR --bgn_iter=0 --fin_iter=$((ITERATION+1)) --interval_iter=100
 
 # Inference, enhanced wavs will be created. 
-ITERATION=10000
-CUDA_VISIBLE_DEVICES=3 python main_dnn.py inference --workspace=$WORKSPACE --tr_snr=$TR_SNR --te_snr=$TE_SNR --n_concat=$N_CONCAT --iteration=$ITERATION
+CUDA_VISIBLE_DEVICES=3 python main_dnn.py inference --workspace=$WORKSPACE --tr_snr=$TR_SNR --te_snr=$TE_SNR --n_concat=$N_CONCAT --iteration=$ITERATION --n_hop=$N_HOP --noise_frame=$N_NOISE_FRAME --visualize
 
 # Calculate PESQ of all enhanced speech. 
 python evaluate.py calculate_pesq --workspace=$WORKSPACE --speech_dir=$TE_SPEECH_DIR --te_snr=$TE_SNR
