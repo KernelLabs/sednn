d0decbb749894f0736012b3d90afad85dc2d8a81
Ubuntu <ubuntu@ip-172-31-14-10.us-west-2.compute.internal>
2018-05-22 23:28:48 +0000

    update runme dir

--- mixture2clean_dnn/runme.sh
+++ mixture2clean_dnn/runme.sh
@@ -2,7 +2,7 @@
 set -e
 
 MINIDATA=0
-if [ $MINIDATA -eq 0 ]; then
+if [ $MINIDATA -eq 1 ]; then
   WORKSPACE="workspace"
   mkdir -p $WORKSPACE
   TR_SPEECH_DIR="mini_data/train_speech_book"
@@ -19,7 +19,7 @@ else
   TR_SPEECH_DIR="/home/ubuntu/data/train_speech"
   TR_NOISE_DIR="/home/ubuntu/data/noise/train3"
   TR_INTERFERE_DIR="/home/ubuntu/data/train_interfere"
-  TE_SPEECH_DIR="/home/ubuntu/data/timit/test_speech"
+  TE_SPEECH_DIR="/home/ubuntu/data/test_speech"
   TE_NOISE_DIR="/home/ubuntu/data/noise/test3"
   TE_INTERFERE_DIR="/home/ubuntu/data/test_interfere"
   echo "Using full data. "
@@ -28,22 +28,22 @@ fi
 
 # Create mixture csv. 
 #rm -r $WORKSPACE
-python prepare_data.py create_mixture_csv --workspace=$WORKSPACE --speech_dir=$TR_SPEECH_DIR --noise_dir=$TR_NOISE_DIR --interfere_dir=$TR_INTERFERE_DIR --data_type=train --magnification=2
-python prepare_data.py create_mixture_csv --workspace=$WORKSPACE --speech_dir=$TE_SPEECH_DIR --noise_dir=$TE_NOISE_DIR --interfere_dir=$TE_INTERFERE_DIR --data_type=test
+#python prepare_data.py create_mixture_csv --workspace=$WORKSPACE --speech_dir=$TR_SPEECH_DIR --noise_dir=$TR_NOISE_DIR --interfere_dir=$TR_INTERFERE_DIR --data_type=train --magnification=2
+#python prepare_data.py create_mixture_csv --workspace=$WORKSPACE --speech_dir=$TE_SPEECH_DIR --noise_dir=$TE_NOISE_DIR --interfere_dir=$TE_INTERFERE_DIR --data_type=test
 
 # Calculate mixture features.
 TR_SNR=10
 TE_SNR=10 
 INTERFER_SNR=10
-python prepare_data.py calculate_mixture_features --workspace=$WORKSPACE --speech_dir=$TR_SPEECH_DIR --noise_dir=$TR_NOISE_DIR --interfere_dir=$TR_INTERFERE_DIR --data_type=train --snr=$TR_SNR --interfere_snr=$INTERFER_SNR
-python prepare_data.py calculate_mixture_features --workspace=$WORKSPACE --speech_dir=$TE_SPEECH_DIR --noise_dir=$TE_NOISE_DIR --interfere_dir=$TE_INTERFERE_DIR --data_type=test --snr=$TE_SNR --interfere_snr=$INTERFER_SNR
+#python prepare_data.py calculate_mixture_features --workspace=$WORKSPACE --speech_dir=$TR_SPEECH_DIR --noise_dir=$TR_NOISE_DIR --interfere_dir=$TR_INTERFERE_DIR --data_type=train --snr=$TR_SNR --interfere_snr=$INTERFER_SNR
+#python prepare_data.py calculate_mixture_features --workspace=$WORKSPACE --speech_dir=$TE_SPEECH_DIR --noise_dir=$TE_NOISE_DIR --interfere_dir=$TE_INTERFERE_DIR --data_type=test --snr=$TE_SNR --interfere_snr=$INTERFER_SNR
 
 # Pack features. 
 N_CONCAT=3
 N_HOP=1
 N_NOISE_FRAME=6
-python prepare_data.py pack_features --workspace=$WORKSPACE --data_type=train --snr=$TR_SNR --n_concat=$N_CONCAT --n_hop=$N_HOP --noise_frame=$N_NOISE_FRAME
-python prepare_data.py pack_features --workspace=$WORKSPACE --data_type=test --snr=$TE_SNR --n_concat=$N_CONCAT --n_hop=$N_HOP --noise_frame=$N_NOISE_FRAME
+#python prepare_data.py pack_features --workspace=$WORKSPACE --data_type=train --snr=$TR_SNR --n_concat=$N_CONCAT --n_hop=$N_HOP --noise_frame=$N_NOISE_FRAME
+#python prepare_data.py pack_features --workspace=$WORKSPACE --data_type=test --snr=$TE_SNR --n_concat=$N_CONCAT --n_hop=$N_HOP --noise_frame=$N_NOISE_FRAME
 
 # Compute scaler. 
 # python prepare_data.py compute_scaler --workspace=$WORKSPACE --data_type=train --snr=$TR_SNR
