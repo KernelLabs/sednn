3bf906532057007c209f1c9d84d9d7f399d2d4a1
xingda <dxingda@gmail.com>
2018-05-31 17:47:07 -0700

    more commit on runme

--- rm_bg_noise_benchmark/runme.sh
+++ rm_bg_noise_benchmark/runme.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 set -e
 
-MINIDATA=1
+MINIDATA=0
 if [ $MINIDATA -eq 1 ]; then
   WORKSPACE="workspace"
   mkdir -p $WORKSPACE
   TR_SPEECH_DIR="mini_data/train_speech"
   TR_NOISE_DIR="mini_data/train_noise"
-  TR_ADAPTUTTER_DIR="mini_data/train_adapt_utt"
   TE_SPEECH_DIR="mini_data/test_speech"
   TE_NOISE_DIR="mini_data/test_noise"
-  TE_ADAPTUTTER_DIR="mini_data/test_adapt_utt"
   echo "Using mini data. "
   ITERATION=1000
 else
@@ -18,29 +16,22 @@ else
   mkdir -p $WORKSPACE
   TR_SPEECH_DIR="/home/ubuntu/data/timit/train"
   TR_NOISE_DIR="/home/ubuntu/data/noise/train3"
-  TR_ADAPTUTTER_DIR="/home/ubuntu/data/train_adapt_utt"
   TE_SPEECH_DIR="/home/ubuntu/data/timit/test2"
   TE_NOISE_DIR="/home/ubuntu/data/noise/test3"
-  TE_ADAPTUTTER_DIR="/home/ubuntu/data/test_adapt_utt"
   echo "Using full data. "
   ITERATION=100000
 fi
 
 # Create mixture csv. 
 #rm -r $WORKSPACE
-python prepare_data.py create_mixture_csv --workspace=$WORKSPACE --speech_dir=$TR_SPEECH_DIR --noise_dir=$TR_NOISE_DIR --data_type=train --magnification=2
+python prepare_data.py create_mixture_csv --workspace=$WORKSPACE --speech_dir=$TR_SPEECH_DIR --noise_dir=$TR_NOISE_DIR --data_type=train --magnification=4
 python prepare_data.py create_mixture_csv --workspace=$WORKSPACE --speech_dir=$TE_SPEECH_DIR --noise_dir=$TE_NOISE_DIR --data_type=test
 
 # Calculate mixture features.
 TR_SNR=10 # training bg noise signal to noise ratio
 TE_SNR=10 # test bg noise signal to noise ratio
-INTERFERE_SNR=10 # interfere speech signal to noise ratio
-python prepare_data.py calculate_mixture_features --workspace=$WORKSPACE --speech_dir=$TR_SPEECH_DIR --noise_dir=$TR_NOISE_DIR --data_type=train --snr=$TR_SNR --interfere_snr=$INTERFERE_SNR
-python prepare_data.py calculate_mixture_features --workspace=$WORKSPACE --speech_dir=$TE_SPEECH_DIR --noise_dir=$TE_NOISE_DIR --data_type=test --snr=$TE_SNR --interfere_snr=$INTERFERE_SNR
-
-# Spectrogram for adaptive utterance
-python prepare_data.py calculate_adaptive_utterance_features --workspace=$WORKSPACE --ada_utt_dir=$TR_ADAPTUTTER_DIR --data_type=train
-python prepare_data.py calculate_adaptive_utterance_features --workspace=$WORKSPACE --ada_utt_dir=$TE_ADAPTUTTER_DIR --data_type=test
+python prepare_data.py calculate_mixture_features --workspace=$WORKSPACE --speech_dir=$TR_SPEECH_DIR --noise_dir=$TR_NOISE_DIR --data_type=train --snr=$TR_SNR
+python prepare_data.py calculate_mixture_features --workspace=$WORKSPACE --speech_dir=$TE_SPEECH_DIR --noise_dir=$TE_NOISE_DIR --data_type=test --snr=$TE_SNR
 
 # Pack features. 
 N_CONCAT=3 # num of frame before current frame to include
